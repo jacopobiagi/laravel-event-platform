effa2ae2ef8ad79c098c1fca976976641fe054d1
SergioBuzi <sergio.buzi96@gmail.com>
2024-03-01 13:30:32 +0100

    config: added index function

--- app/Http/Controllers/Auth/Admin/EventController.php
+++ app/Http/Controllers/Auth/Admin/EventController.php
@@ -17,7 +17,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        $events = Event :: all();
+        return view('events.index', compact('events'));
     }
 
     /**
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,4 +2,14 @@
 
 @section('content')
     <h1>Events</h1>
+
+    <ul>
+        @foreach ($events as $event)   
+            <li>
+                {{$event -> name}}
+            </li>       
+        @endforeach
+    </ul>
+
+    
 @endsection
