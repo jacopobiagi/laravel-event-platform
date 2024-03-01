20202bc6ebbd92b8d480828aa642ce5607af393e
SergioBuzi <sergio.buzi96@gmail.com>
2024-03-01 15:33:14 +0100

    config: add create

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create</h1>
+
+    <h1>Crea un nuovo evento</h1>
+
+        <form method="POST">
+
+            @csrf
+            @method('POST')
+
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name">
+            <br>
+            <label for="description">description</label>
+            <input type="text" name="description" id="description">
+            <br>
+            <label for="start_date">Data inizio</label>
+            <input type="date" name="start_date" id="start_date">
+            <br>
+            <label for="end_date">Data fine</label>
+            <input type="date" name="end_date" id="end_date">
+            <br>
+            <label for="tags">Tags</label>
+            <br>
+            @foreach($tags as $tag)
+                <div>
+                    <input type="checkbox" name="tags[]" value="{{ $tag -> id }}" id="tag{{ $tag -> id }}">
+                    <label for="tag{{ $tag -> id }}">{{ $tag -> title }}</label>
+                    <br>
+                </div>
+            @endforeach
+            <input type="submit" value="invia">
+
+
+        </form>
+
 @endsection
+
+<style>
+    form{
+        display: block;
+    }
+</style>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [EventController :: class, 'index']) 
     ->name('events.index');
 
+Route::get('/events/create', [EventController :: class, 'create']) 
+    ->name('events.create');
+
+Route :: post('/events/create', [EventController :: class, 'store'])
+    ->name('events.store');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
