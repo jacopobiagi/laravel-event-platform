ac50245c2f363c22b400ec76f13f89c65ed32b96
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-03-01 18:02:23 +0100

    config: create auth

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,6 @@ Route::get('/', [EventController :: class, 'index'])
     ->name('events.index');
 
 
-Route::get('/', [EventController :: class, 'index']) 
-    -> name('events.index');
-
-Route::get('/events/{id}', [EventController :: class, 'show'])
-    -> name('events.show');
-
-
-    
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -37,13 +29,15 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // rotte events protette da auth
-   
-    Route::get('/events/create', [EventController :: class, 'create']) 
+
+    Route::get('/events/create', [EventController :: class, 'create'])
         ->name('events.create');
 
     Route :: post('/events/create', [EventController :: class, 'store'])
         ->name('events.store');
-        
+
+        Route::get('/events/{id}', [EventController :: class, 'show'])
+    -> name('events.show');
 
     Route :: get('/events/{id}/edit', [EventController :: class, 'edit'])
         -> name('events.edit');
@@ -51,7 +45,7 @@ Route::middleware('auth')->group(function () {
     Route :: put('/events/{id}', [EventController :: class, 'update'])
         -> name('events.update');
 
-    Route::delete('/users/{id}', [EventController :: class, 'destroy']) 
+    Route::delete('/users/{id}', [EventController :: class, 'destroy'])
         ->name('events.destroy');
 
 
