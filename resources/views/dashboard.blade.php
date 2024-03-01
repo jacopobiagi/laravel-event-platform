5afd7986aa505d70795ad4ca1405ac09ccc5a28f
SergioBuzi <sergio.buzi96@gmail.com>
2024-03-01 11:52:03 +0100

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        {{ __('You are logged in!') }} 
                     </div>
                 </div>
             </div>
